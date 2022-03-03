8eb5dabb0287e2e21dc8855f15fa79973f18d725
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2022-03-03 11:07:49 -0500

    Working flash chip programmer

--- Hardware/rv32i_programmer/prog.py
+++ Hardware/rv32i_programmer/prog.py
@@ -1,7 +1,11 @@
 import time
 import serial
 import struct
+import logging
 from tqdm import tqdm
+
+
+PAGE_COUNT = 8192
 
 
 class commands:
@@ -29,6 +33,9 @@ class packet_t:
     def __init__(self, buffer=b"", cmd=commands.NUL):
         self.buffer = buffer
         self.cmd = cmd
+
+    def __str__(self):
+        return f"cmd={self.cmd} buffer={self.buffer.hex()}"
 
 
 def crc16(data=b""):
@@ -73,104 +80,115 @@ def recv_packet():
 
     if crc16(payload) != crc:
         command = command.NAK
+        logging.warning(f"CRC mismatch {crc16(payload)} != {crc}")
     if ser.read(1) != struct.pack("<B", packet_t.FRAME_END_FLAG):
         command = commands.ERR
+        logging.warning("Frame end flag error")
     if command == commands.NUL:
+        logging.warning("NULL command error")
         command = commands.ERR
 
+    logging.debug(f"{command=} {length=} {payload=} {crc=}")
     return packet_t(payload, ord(command))
 
 
 def acknowledge():
     resp = recv_packet()
     if resp.cmd != commands.ACK:
-        raise Exception(f"SYN failed: ACK != {res.cmd}")
+        logging.error(f"cmd={res.cmd} buffer={res.buffer.hex()}")
+        raise Exception("SYN failed")
 
 
 def send_command(command, buffers: list[bytes] = [], recv_packet_len=0):
     send_packet(packet_t(cmd=command))
     acknowledge()
 
-    if len(buffers) > 0:
-        [send_packet(packet_t(cmd=commands.SYN, buffer=buf)) for buf in buffers]
-    if recv_packet_len > 0:
-        recv = [recv_packet() for _ in range(recv_packet_len)]
-        # recv = [packet for packet in recv if packet.cmd == command]
-    else:
-        recv = []
+    for buf in buffers:
+        send_packet(packet_t(cmd=commands.SYN, buffer=buf))
+
+    recv = []
+    for _ in range(recv_packet_len):
+        recv.append(recv_packet())
 
     acknowledge()
     return recv
 
 
 def main(ser):
-    print("Init serial communications")
+    logging.info("Init serial communications")
     acknowledge()
 
     #
     # CONFIRM SPI COMMUNICATIONS
     #
-    print("Getting flash chip ID... ", end="")
+    logging.info("Getting flash chip ID... ")
     (resp,) = send_command(command=commands.READ_JEDEC_ID, recv_packet_len=1)
     jedec = resp.buffer
     print(f"{jedec.hex()} ", end="")
     if b"\xef\x40" in jedec[::-1]:
         print("OK")
     else:
+        logging.error(f"ID= {jedec.hex()}")
         raise Exception("Flash chip ID error")
 
     #
     # ERASE FLASH
     #
-    print("Erasing flash chip... ", end="")
+    logging.info("Erasing flash chip... ")
     start = time.time()
     send_command(command=commands.CHIP_ERASE)
     if (time.time() - start) > 1:
         print("OK")
     else:
+        logging.error(f"Erase time={time.time() - start}")
         raise Exception("Flash chip erase error")
-
-    return
 
     #
     # WRITE ALL PAGES
     #
-    print("Writing pages of flash chip... ")
-    # for page in tqdm(range(PAGE_COUNT)):
-    for page in range(PAGE_COUNT):
-        # checks = send_command(
-        #     command=PAGE_PROGRAM,
-        #     recv_len=3,
-        #     buffers=[struct.pack("<H", page), bytes_buffer],
-        # )
-        # page_check, crc_check = struct.unpack("<HB", checks)
-        sync()
-        ser.write(struct.pack("<B", PAGE_PROGRAM))
-        ser.write(struct.pack("<H", page))
-        ser.write(bytes_buffer)
+    bytes_buffer = bytes([i for i in range(256)])
 
-        (page_check,) = struct.unpack("<H", ser.read(2))
-        (crc_check,) = struct.unpack("<B", ser.read(1))
-        bytes_check = ser.read(256)
+    logging.info("Writing pages of flash chip... ")
+    for page in tqdm(range(PAGE_COUNT)):
+        (a,) = send_command(
+            command=commands.PAGE_PROGRAM,
+            buffers=[struct.pack("<H", page), bytes_buffer],
+            recv_packet_len=1,
+        )
+        (page_check,) = struct.unpack("<H", a.buffer)
+        if page_check != page:
+            logging.error(f"Page check error {page_check} != {page}")
+            raise Exception("Page check error")
+    print("OK")
 
-        crc_page = crc8(0, bytes_buffer)
+    #
+    # CHECK ALL PAGES
+    #
+    logging.info("Checking pages of flash chip... ")
+    for page in tqdm(range(PAGE_COUNT)):
+        (a, b) = send_command(
+            command=commands.READ_DATA,
+            buffers=[struct.pack("<H", page)],
+            recv_packet_len=2,
+        )
+        (page_check,) = struct.unpack("<H", a.buffer)
+        if page_check != page:
+            logging.error(f"Page check error {page_check} != {page}")
+            raise Exception("Page check error")
 
-        print(f"{page_check=} {page}")
-        print(f"{crc_check=} {crc_page=}")
-        print(f"{bytes_check}")
+        if b.buffer != bytes_buffer:
+            logging.error(
+                f"Page data check error {b.buffer.hex()} != {bytes_buffer.hex()}"
+            )
+            raise Exception("Page data check error")
+    print("OK")
 
-        # if crc_check != crc_page:
-        #     raise Exception(
-        #         f"Data integrity error, CRC mismatch: {crc_page} != {crc_check}"
-        #     )
-        # if page_check != page:
-        #     raise Exception(
-        #         f"Data integrity error, page mismatch: {page} != {page_check}"
-        #     )
-
-    ser.close()
+    logging.info("DONE")
 
 
 if __name__ == "__main__":
-    ser = serial.Serial("COM3", 500000)
-    main(ser)
+    # logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+
+    with serial.Serial("COM4", 500000) as ser:
+        main(ser)
--- Hardware/rv32i_programmer/src/main.cpp
+++ Hardware/rv32i_programmer/src/main.cpp
@@ -26,26 +26,66 @@ void setup()
 void loop()
 {
   data_packet = pyserial::comm::recv_packet(data_packet);
-  pyserial::comm::send_packet(ack_packet);
-  switch (data_packet->cmd)
+  if (data_packet->cmd == pyserial::NAK || data_packet->cmd == pyserial::ERR)
   {
-  case pyserial::_READ_JEDEC_ID:
+    pyserial::comm::send_packet(nak_packet);
+    return;
+  }
+  else
+  {
+    pyserial::comm::send_packet(ack_packet);
+  }
+
+  if (data_packet->cmd == pyserial::_READ_JEDEC_ID)
   {
     uint32_t id = flash.getChipInfo();
     pyserial::packet_t *resp = new pyserial::packet_t(
-        pyserial::_READ_JEDEC_ID,
+        pyserial::SYN,
         (uint8_t *)&id, 4);
     pyserial::comm::send_packet(resp);
     delete resp;
   }
-  break;
-  case pyserial::_CHIP_ERASE:
+  else if (data_packet->cmd == pyserial::_CHIP_ERASE)
   {
     flash.chipErase();
+    // pyserial::comm::send_packet(ack_packet);
   }
-  break;
-  default:
-    break;
+  else if (data_packet->cmd == pyserial::_PAGE_PROGRAM)
+  {
+    data_packet = pyserial::comm::recv_packet(data_packet);
+    uint16_t page = data_packet->buf[0] | (data_packet->buf[1] << 8);
+    data_packet = pyserial::comm::recv_packet(data_packet);
+
+    pyserial::packet_t *resp = new pyserial::packet_t(
+        pyserial::SYN,
+        (uint8_t *)&page, 2);
+    pyserial::comm::send_packet(resp);
+    delete resp;
+
+    flash.initWrite(page, 0);
+    SPI.transfer(data_packet->buf, data_packet->len);
+    flash.endWrite();
+  }
+  else if (data_packet->cmd == pyserial::_READ_DATA)
+  {
+    data_packet = pyserial::comm::recv_packet(data_packet);
+    uint16_t page = data_packet->buf[0] | (data_packet->buf[1] << 8);
+
+    pyserial::packet_t *resp = new pyserial::packet_t(
+        pyserial::SYN,
+        (uint8_t *)&page, 2);
+    pyserial::comm::send_packet(resp);
+    delete resp;
+
+    data_packet->cmd = pyserial::SYN;
+    data_packet->len = PAGE_SIZE;
+    flash.initRead(page, 0);
+    for (int i = 0; i < PAGE_SIZE; i++)
+    {
+      data_packet->buf[i] = flash.readByte();
+    }
+    flash.endRead();
+    pyserial::comm::send_packet(data_packet);
   }
 
   pyserial::comm::send_packet(ack_packet);
